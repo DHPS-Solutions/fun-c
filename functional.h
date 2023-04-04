71614690a250eb4afb8630f9ec91a7772d4d4ae2
callumgran <callum.gran@gmail.com>
2023-04-04 19:23:06 +0200

    well on the pipe way

--- functional.h
+++ functional.h
@@ -5,6 +5,27 @@
 #ifndef FUNCTIONAL_H
 #define FUNCTIONAL_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * Enums to hold the different types of functions.
+ */
+#define MAP_ENUM 1
+#define FOREACH_ENUM 2
+#define FILTER_ENUM 3
+#define REDUCE_ENUM 4
+
+/**
+ * Struct to hold an array.
+ * @param arr The array.
+ * @param len The length of the array.
+ */
+struct array_t {
+    int *arr;
+    int len;
+};
+
 /**
  * Macro to create an lambda function.
  * @param return_type The return type of the lambda function.
@@ -29,5 +50,69 @@
  */
 #define ANON(return_type, func_args, func_body, ...)\
     LAMBDA(return_type, func_args, func_body)(__VA_ARGS__)
+
+/**
+ * Macro to create a consumer foreach loop.
+ * @param array The array to iterate over.
+ * @param func The function to call for each element.
+ */
+#define FOREACH(array, func)\
+({\
+    for (int i = 0; i < array.len; i++)\
+        func(array.arr[i]);\
+})
+
+/**
+ * Macro to iterate and create a map.
+ * @param array The array to iterate over.
+ * @param func The function to call for each element.
+ */
+#define MAP(array, func)\
+({\
+    int *map_arr = malloc(sizeof(int) * array.len);\
+    struct array_t mapped = {\
+        .arr = map_arr,\
+        .len = array.len\
+    };\
+    for (int i = 0; i < array.len; i++)\
+        mapped.arr[i] = func(array.arr[i]);\
+    mapped;\
+})
+
+/**
+ * Function to pipe a value into a function.
+ * @param value The value to pipe.
+ * @param ... The functions to pipe the value through.
+ * @return The return value of the function.
+ */
+int int_pipe(int value, ...) 
+{
+    va_list args;
+    va_start(args, value);
+    
+    int (*func)(int);
+
+    while ((func = va_arg(args, int (*)(int))) != NULL)
+        value = func(value);
+
+    va_end(args);
+    
+    return value;
+}
+
+/**
+ * Macro to pipe a value into a function.
+ * @param value The value to pipe.
+ * @param ... The functions to pipe the value through.
+ * @return The return value of the function.
+ */
+// #define PIPE(value, ...)\
+// ({\
+//     va_list args;\
+//     va_start(args, value);\
+//     int choice;\
+//     while ((choice = va_arg(args, void *)) != 0) \
+//         va_arg(args, void *)
+// })
 
 #endif
--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 
 #include "functional.h"
 
+void print_int_array(int *arr, int len) {
+    for (int i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+}
+
 int main(int argc, char *argv[]) 
 {
     // Lambda functions
@@ -36,4 +43,45 @@ int main(int argc, char *argv[])
     printf("%f\n", ANON(double, (double a, float b, int c), { 
         return a + b + c;
     }, 99.2, 42.3, 44));
+
+    // For each
+    struct array_t arr = {
+        .arr = (int []) { 1, 2, 3, 4, 5 },
+        .len = 5
+    };
+
+    void (*print_int)(int) = LAMBDA(void, (int i), {
+        printf("%d ", i);
+    });
+
+    FOREACH(arr, print_int);
+
+    printf("\n");
+
+    // Map
+    int *mapped = MAP(arr, LAMBDA(int, (int i), {
+        return i * 2;
+    })).arr;
+
+    printf("Mapped: ");
+    print_int_array(mapped, 5);
+    
+    free(mapped);
+
+    // Pipe
+    int (*add_one)(int) = LAMBDA(int, (int i), {
+        return i + 1;
+    });
+
+    int (*add_two)(int) = LAMBDA(int, (int i), {
+        return i + 2;
+    });
+
+    int (*add_three)(int) = LAMBDA(int, (int i), {
+        return i + 3;
+    });
+
+    printf("Piped: %d\n", int_pipe(1, add_one, add_two, add_three));
+
+    return 0;
 }
