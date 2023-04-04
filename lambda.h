ab20839bf6ba3df8166227e8652e7b3a1ec5f08e
callumgran <callum.gran@gmail.com>
2023-04-04 15:50:30 +0200

    shortened and documented

--- lambda.h
+++ lambda.h
@@ -1,22 +1,24 @@
 /**
  * Authored by Callum Gran 2023.
- * Simple macro's to allow for lambda functions in C.
+ * Simple macro to allow for lambda functions in C.
  */
 
 #ifndef LAMBDA_H
 #define LAMBDA_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-
-#define __lambda_fn__(return_type, func_args, func_body)\
+/**
+ * Macro to create a lambda function.
+ * @param return_type The return type of the lambda function.
+ * @param func_args The arguments of the lambda function.
+ * @param func_body The body of the lambda function.
+ * @param ... The arguments to pass to the lambda function.
+ * @return The return value of the lambda function.
+ */
+#define LAMBDA(return_type, func_args, func_body, ...)\
 ({\
     return_type __fn_t__ func_args\
     func_body\
     &__fn_t__;\
-})
-
-#define LAMBDA(return_type, func_args, func_body, ...)\
-    (return_type) __lambda_fn__(return_type, func_args, func_body)(__VA_ARGS__)
+})(__VA_ARGS__)
 
 #endif
--- main.c
+++ main.c
@@ -7,4 +7,5 @@ int main(int argc, char *argv[])
 {
     printf("%s\n", LAMBDA(char *, (char *s), { return s; }, "HELLO WORLD!"));
     printf("%d\n", LAMBDA(int, (int a, int b, int c), { return a + b + c; }, 43, 42, 44));
+    printf("%f\n", LAMBDA(double, (double a, float b, int c), { return a + b + c; }, 99.2, 42.3, 44));
 }
