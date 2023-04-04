b7518f4ab021deb7edf630fb68823809dc1cea18
callumgran <callum.gran@gmail.com>
2023-04-04 16:25:49 +0200

    fixed to correct definitions and split lambdas and anonymous functions

--- functional.h
+++ functional.h
@@ -6,6 +6,20 @@
 #define FUNCTIONAL_H
 
 /**
+ * Macro to create an lambda function.
+ * @param return_type The return type of the lambda function.
+ * @param func_args The arguments of the lambda function.
+ * @param func_body The body of the lambda function.
+ * @return A pointer to the lambda function.
+ */
+#define LAMBDA(return_type, func_args, func_body)\
+({\
+    return_type __fn_t__ func_args\
+    func_body\
+    &__fn_t__;\
+})
+
+/**
  * Macro to create an anonymous function function.
  * @param return_type The return type of the anonymous function.
  * @param func_args The arguments of the anonymous function.
@@ -14,10 +28,6 @@
  * @return The return value of the anonymous function.
  */
 #define ANON(return_type, func_args, func_body, ...)\
-({\
-    return_type __fn_t__ func_args\
-    func_body\
-    &__fn_t__;\
-})(__VA_ARGS__)
+    LAMBDA(return_type, func_args, func_body)(__VA_ARGS__)
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "functional.h"
 
 int main(int argc, char *argv[]) 
 {
-    printf("%s\n", ANON(char *, (char *s), { return s; }, "HELLO WORLD!"));
-    printf("%d\n", ANON(int, (int a, int b, int c), { return a + b + c; }, 43, 42, 44));
-    printf("%f\n", ANON(double, (double a, float b, int c), { return a + b + c; }, 99.2, 42.3, 44));
+    // Lambda functions
+    int (*combine_two)(int, int) = LAMBDA(int, (int a, int b), {
+        return a + b; 
+    });
+
+    printf("%d\n", combine_two(1, 2));
+    
+    char *(*to_upper)(char *) = LAMBDA(char *, (char *s), {
+        char *str = strdup(s);
+        for (int i = 0; i < strlen(str); i++) {
+            if((str[i] > 96) && (str[i] < 123)) str[i] ^=0x20;
+        }
+        
+        return str;
+    });
+
+    printf("%s\n", to_upper("hello world!"));
+
+    // Anonymous functions
+    printf("%s\n", ANON(char *, (char *s), { 
+        return s; 
+    }, "HELLO WORLD!"));
+
+    printf("%d\n", ANON(int, (int a, int b, int c), { 
+        return a + b + c;
+    }, 43, 42, 44));
+
+    printf("%f\n", ANON(double, (double a, float b, int c), { 
+        return a + b + c;
+    }, 99.2, 42.3, 44));
 }
